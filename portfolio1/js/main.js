9b721406c4ff272437ccab2dda09f2ec0d05f20b
armarce <ikkimasie@gmail.com>
2022-08-09 03:14:47 -0400

    Add Readme

--- portfolio1/js/main.js
+++ portfolio1/js/main.js
@@ -1,18 +1,54 @@
 
-document.querySelectorAll('header nav ul li a').forEach(a => {
-    a.addEventListener('click', event => {
-        document.querySelector('header nav ul').style.left = '-100%';
+const list = document.querySelector('header nav ul');
+const links = document.querySelectorAll('header nav ul li a');
+
+links.forEach(a => {
+    a.addEventListener('click', e => {
+        list.style.left = '-100%';
         document.getElementById("checkbox").checked = false;
     });
 });
 
-document.getElementById("checkbox").addEventListener("change", (e) => {
+document.getElementById("checkbox").addEventListener("change", e => {
     
-    if(e.target.checked){
-        
-        document.querySelector('header nav ul').style.left = '0';
-    }else{
-        document.querySelector('header nav ul').style.left = '-100%';
+    e.target.checked ? list.style.left = '0': list.style.left = '-100%';
+
+});
+
+
+/*
+
+Carousel based on:
+
+https://codepen.io/Nathan_James/pen/BvNLzO
+
+*/
+
+const carousel = document.querySelector("#experience ul");
+const card = carousel.querySelector(".exp");
+const leftButton = document.querySelector(".btn-prev");
+const rightButton = document.querySelector(".btn-next");
+
+const carouselWidth = 200;
+const cardStyle = card.currentStyle || window.getComputedStyle(card)
+const cardMarginRight = 0;
+
+
+const cardCount = carousel.querySelectorAll(".exp").length;
+
+let offset = 0;
+const max = -(cardCount  * carouselWidth + (cardMarginRight * cardCount) - carouselWidth - cardMarginRight);
+
+leftButton.addEventListener("click", () => {
+  if(offset !== 0){
+        offset += carouselWidth;
+        carousel.style.transform = `translateX(${offset}px)`;
     }
 });
 
+rightButton.addEventListener("click", () => {
+    if(offset !== max){
+        offset -= carouselWidth;
+        carousel.style.transform = `translateX(${offset}px)`;
+    }
+});
